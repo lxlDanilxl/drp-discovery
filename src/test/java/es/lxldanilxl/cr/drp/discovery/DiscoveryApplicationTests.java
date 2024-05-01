75a41a84ca4ffcb3399d0ff6833f40480323f783
Rogelio Daniel Franco Martínez <r.d.franco@udc.es>
2024-05-01 11:40:37 +0200

    Initial commit

---
+++ src/test/java/es/lxldanilxl/cr/drp/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package es.lxldanilxl.cr.drp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
